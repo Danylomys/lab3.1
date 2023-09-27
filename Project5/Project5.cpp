bf79d4ac69cbd3a0b3fd8686cdfbf107d495a1a2
Danylo <danylo.mysyshyn.ri.2023@lpnu.ua>
2023-09-27 17:03:57 +0300

    last

--- Project5/Project5.cpp
+++ Project5/Project5.cpp
@@ -24,7 +24,7 @@ int main()
 		B = 4 * pow(x, 7) - pow(x, 5) + pow(x, 3) - 2;
 	if (4 <= x && x < 7)
 		B = atan(abs((x)+1) / 2) + 8.3 * x;
-	if (x >= 1)
+	if (x >= 7)
 		B = log(abs(2 * x + exp(4 * x + 1)));
 	
 	y = A + B;
@@ -32,6 +32,17 @@ int main()
 	cout << endl;
 	cout << "1) y = " << y << endl;
 	
+	if (x < 4)
+		B = 4 * pow(x, 7) - pow(x, 5) + pow(x, 3) - 2;
+	else
+		if (x >= 7)
+			B = log(abs(2 * x + exp(4 * x + 1)));
+		else
+			B = atan(abs((x)+1) / 2) + 8.3 * x;
+	y = A + B;
+	
+	cout << "2) y = " << y << endl;
 	cin.get();
+	
 	return 0;
 }
